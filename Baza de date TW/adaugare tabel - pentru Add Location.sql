1c9b09fdc8660e6b3fec3686201622fd1ce0ef28
SirA1ex <ualex95@yahoo.com>
2016-05-06 18:19:30 +0300

    update v3.5
    - adaugata optiune Add Location - Alina

---
+++ Baza de date TW/adaugare tabel - pentru Add Location.sql
@@ -0,0 +1,13 @@
+CREATE TABLE locatii_adaugate (
+  nume VARCHAR2(100), 
+  categorie VARCHAR2(100), 
+  latitudine VARCHAR2(100), 
+  longitudine VARCHAR2(100), 
+  oras VARCHAR2(100), 
+  tara VARCHAR2(100), 
+  descriere VARCHAR2(100)
+  )
+  
+/
+show errors;
+COMMIT;
