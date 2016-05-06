a95cdc97ec145fee8e11e90c2925944aefc4c208
SirA1ex <ualex95@yahoo.com>
2016-05-06 14:04:28 +0300

    demo tabel locatii v1.1

--- Baza de date TW/Tabel_locatii.sql
+++ Baza de date TW/Tabel_locatii.sql
@@ -1,6 +1,5 @@
 
-CREATE TABLE locatii_personale(
-  proprietar VARCHAR2(30),
+CREATE TABLE locatii(
   nume VARCHAR2(20),
   latitudine NUMBER(20,10),
   longitudine NUMBER(20,10),
@@ -11,13 +10,7 @@ CREATE TABLE locatii_personale(
   )
 /
 CREATE TABLE locatii_favorite(
-  proprietar VARCHAR2(30),
-  nume VARCHAR2(20),
-  latitudine NUMBER(20,10),
-  longitudine NUMBER(20,10),
-  oras VARCHAR2(20),
-  descriere VARCHAR2(30),
-  tip_res VARCHAR2(15),
+  id_user INTEGER,
   id_res INTEGER
   )
 /
