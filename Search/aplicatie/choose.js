191777dafc34ea26fad6917afa3b56bc1f23793f
SirA1ex <ualex95@yahoo.com>
2016-05-07 16:58:13 +0300

    Search

--- Search/aplicatie/choose.js
+++ Search/aplicatie/choose.js
@@ -1,9 +1,8 @@
 var VAR;
 function choose(choice){
     localStorage.setItem("tip", choice);
-}
-
-function test(click){
     alert("You chossed " + localStorage.getItem("tip"));
     window.location='home.html';
 }
+
+
